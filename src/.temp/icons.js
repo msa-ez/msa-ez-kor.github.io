543e96b79689f4d4415da3bfcf9bcb30712037e0
kibumpark <top5w5@naver.com>
2024-01-05 17:05:17 +0900

    gitignore

---
+++ src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+  "touchiconMimeType": "image/png",
+  "faviconMimeType": "image/png",
+  "precomposed": false,
+  "touchicons": [
+    {
+      "width": 76,
+      "src": "/assets/static/src/assets/favicon.png?width=76&key=da2a65e"
+    },
+    {
+      "width": 152,
+      "src": "/assets/static/src/assets/favicon.png?width=152&key=da2a65e"
+    },
+    {
+      "width": 120,
+      "src": "/assets/static/src/assets/favicon.png?width=120&key=da2a65e"
+    },
+    {
+      "width": 167,
+      "src": "/assets/static/src/assets/favicon.png?width=167&key=da2a65e"
+    },
+    {
+      "width": 180,
+      "src": "/assets/static/src/assets/favicon.png?width=180&key=da2a65e"
+    }
+  ],
+  "favicons": [
+    {
+      "width": 16,
+      "src": "/assets/static/src/img/logo.png?width=16&key=45a3c8c"
+    },
+    {
+      "width": 32,
+      "src": "/assets/static/src/img/logo.png?width=32&key=45a3c8c"
+    },
+    {
+      "width": 96,
+      "src": "/assets/static/src/img/logo.png?width=96&key=45a3c8c"
+    }
+  ]
+}
