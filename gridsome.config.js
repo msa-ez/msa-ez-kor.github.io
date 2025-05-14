f2b04eab099c2db488e05372863d72e92d0d33be
kibum0405 <59447401+kibum0405@users.noreply.github.com>
2025-05-14 11:20:26 +0900

    왼쪽 메뉴 애그리거트 설계 제거(주석)

--- gridsome.config.js
+++ gridsome.config.js
@@ -78,14 +78,14 @@ module.exports = {
                 secondTitle: 'AI 기반 코드 구현',
                 secondItem: '/tool/si-gpt/'
               },
-              {
-                secondTitle: '애그리거트 설계',
-                secondItem: '/tool/aggregate-design/'
-              },
               // {
-              //   secondTitle: 'DDL to 이벤트스토밍',
-              //   secondItem: '/tool/ddl-to-eventstorming/'
+              //   secondTitle: '애그리거트 설계',
+              //   secondItem: '/tool/aggregate-design/'
               // },
+              {
+                secondTitle: 'DDL to 이벤트스토밍',
+                secondItem: '/tool/ddl-to-eventstorming/'
+              },
               {
                 secondTitle: 'BC & 도메인 설계 AI',
                 secondItem: '/tool/bc-domain-gen/'
